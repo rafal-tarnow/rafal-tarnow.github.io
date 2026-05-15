ea40389ffe5a839d78d012a67888e6e58e8a0525
Tomasz Pecak <tom85.pecak@gmail.com>
2026-05-15 21:53:04 +0200

    translate login page and user avatar menu

--- english_learner/flutter/flutter_bootstrap.js
+++ english_learner/flutter/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "967552695" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3511880102" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
